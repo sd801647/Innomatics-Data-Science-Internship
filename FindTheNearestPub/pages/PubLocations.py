5ac5475710c84389176563f321e7026bb69ef207
SUBHAMAY DHARA <63958713+sd801647@users.noreply.github.com>
2023-04-22 10:51:41 +0530

    Update PubLocations.py

--- FindTheNearestPub/pages/PubLocations.py
+++ FindTheNearestPub/pages/PubLocations.py
@@ -6,8 +6,10 @@ import app
 
 # absolute path to this file
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
 # absolute path of directory_of_interest
-dir_of_interest = os.path.join(FILE_DIR, "data")
+dir_of_interest = os.path.join(PARENT_DIR, "data")
 DATA_PATH = os.path.join(dir_of_interest, "open_pubs.csv")
 df=pd.read_csv(DATA_PATH)
 
